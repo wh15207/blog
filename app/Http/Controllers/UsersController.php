ea04baeec186ae54d9f22b2253ad0b5429ef2429
wh15207 <wh15207@163.com>
2018-07-19 06:29:35 +0000

    finsh login&&logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests;
+use Auth;
+
 class UsersController extends Controller
 {
     public function create(){
@@ -16,7 +18,7 @@ class UsersController extends Controller
     public function store(Request $request){
         $this->validate($request,[
             'name' => 'required|max:50',
-            'email' => 'required|unique:users|max:255',
+            'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
         $user = User::create([
@@ -24,6 +26,7 @@ class UsersController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
+        Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show',[$user]);
     }
